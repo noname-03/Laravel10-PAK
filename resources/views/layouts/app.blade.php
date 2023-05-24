f027f4fe598819b1e657d4344c526cd48474b2f4
Fahrurrozi Arul <fahrurrozi.arul3@gmail.com>
2023-05-24 11:36:12 +0700

    add template admin

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-topbar-color="dark">
+
+<head>
+    <meta charset="utf-8" />
+    <title>OPAK || @yield('title')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Coderthemes" name="author" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="/assets/images/favicon.ico">
+
+    <!-- Theme Config Js -->
+    <script src="/assets/js/head.js"></script>
+
+    <!-- Bootstrap css -->
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" id="app-style" />
+
+    <!-- App css -->
+    <link href="/assets/css/app.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- Icons css -->
+    <link href="/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+
+    @stack('styles')
+</head>
+
+<body>
+
+    <!-- Begin page -->
+    <div id="wrapper">
+
+
+        <!-- ========== Menu ========== -->
+        @include('layouts.partials._sidebar')
+        <!-- ========== Left menu End ========== -->
+
+
+
+
+
+        <!-- ============================================================== -->
+        <!-- Start Page Content here -->
+        <!-- ============================================================== -->
+
+        <div class="content-page">
+
+            <!-- ========== Topbar Start ========== -->
+            @include('layouts.partials._topbar')
+            <!-- ========== Topbar End ========== -->
+
+            <div class="content">
+
+                <!-- Start Content-->
+                @yield('content')
+                <!-- container -->
+
+            </div> <!-- content -->
+
+            <!-- Footer Start -->
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div>
+                                <script>
+                                    document.write(new Date().getFullYear())
+                                </script> © Ubold - <a href="https://coderthemes.com/"
+                                    target="_blank">Coderthemes.com</a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="d-none d-md-flex gap-4 align-item-center justify-content-md-end footer-links">
+                                <a href="javascript: void(0);">About</a>
+                                <a href="javascript: void(0);">Support</a>
+                                <a href="javascript: void(0);">Contact Us</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <!-- end Footer -->
+
+        </div>
+
+        <!-- ============================================================== -->
+        <!-- End Page content -->
+        <!-- ============================================================== -->
+
+
+    </div>
+    <!-- END wrapper -->
+
+    <!-- Vendor js -->
+    <script src="/assets/js/vendor.min.js"></script>
+
+    <!-- App js -->
+    <script src="/assets/js/app.min.js"></script>
+
+    @stack('scripts')
+</body>
+
+</html>
---
+++ resources/views/layouts/partials/_sidebar.blade.php
@@ -0,0 +1,82 @@
+<div class="app-menu">
+
+    <!-- Brand Logo -->
+    <div class="logo-box">
+        <!-- Brand Logo Light -->
+        <a href="index.html" class="logo-light">
+            <img src="assets/images/logo-light.png" alt="logo" class="logo-lg">
+            <img src="assets/images/logo-sm.png" alt="small logo" class="logo-sm">
+        </a>
+
+        <!-- Brand Logo Dark -->
+        <a href="index.html" class="logo-dark">
+            <img src="assets/images/logo-dark.png" alt="dark logo" class="logo-lg">
+            <img src="assets/images/logo-sm.png" alt="small logo" class="logo-sm">
+        </a>
+    </div>
+
+    <!-- menu-left -->
+    <div class="scrollbar">
+
+        <!--- Menu -->
+        <ul class="menu">
+
+            <li class="menu-title">Menu</li>
+            <li class="menu-item">
+                <a href="{{route('home')}}" class="menu-link">
+                    <span class="menu-icon"><i data-feather="home"></i></span>
+                    <span class="menu-text"> Home </span>
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="#" class="menu-link">
+                    <span class="menu-icon"><i data-feather="book"></i></span>
+                    <span class="menu-text"> PAK </span>
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="#" class="menu-link">
+                    <span class="menu-icon"><i data-feather="printer"></i></span>
+                    <span class="menu-text"> Cetak </span>
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="#" class="menu-link">
+                    <span class="menu-icon"><i data-feather="trending-up"></i></span>
+                    <span class="menu-text"> Statistik </span>
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="#" class="menu-link">
+                    <span class="menu-icon"><i data-feather="download"></i></span>
+                    <span class="menu-text"> Unduh </span>
+                </a>
+            </li>
+
+            {{-- Referensi --}}
+            <li class="menu-title">Referensi</li>
+
+            <li class="menu-item">
+                <a href="#" class="menu-link">
+                    <span class="menu-icon"><i data-feather="clipboard"></i></span>
+                    <span class="menu-text"> Angka Kredit </span>
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="#" class="menu-link">
+                    <span class="menu-icon"><i data-feather="user"></i></span>
+                    <span class="menu-text"> Akun </span>
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="#" class="menu-link">
+                    <span class="menu-icon"><i class="icon-question"></i></span>
+                    <span class="menu-text"> FAQ </span>
+                </a>
+            </li>
+
+        </ul>
+        <!--- End Menu -->
+        <div class="clearfix"></div>
+    </div>
+</div>
